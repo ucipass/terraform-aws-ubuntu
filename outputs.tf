8a2cb014a169a5090072c8ca28929d4a6a84734d
ucipass@gmail.com <ucipass@gmail.com>
2021-09-05 12:36:52 -0500

    v0.0.5

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "public_ip" {
-  value = aws_instance.EC2_1.public_ip
+  value = aws_instance.public_ip.public_ip
 }
 output "private_ip" {
-  value = aws_instance.EC2_1.private_ip
+  value = aws_network_interface.eth0.private_ip
 }
 output "private_ip2" {
   value = aws_network_interface.eth1.private_ip
